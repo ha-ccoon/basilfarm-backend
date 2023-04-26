8ec5335d1f272dc580ac33cb23318b728a215238
minha sohn <codeminhasohn@gmail.com>
2023-04-26 11:05:43 +0900

    [DEL] config/index 파일 정리

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-import express from "express";
-import { port } from "./config/index.js";
-import apiRouter from "./routes/index.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import apiRouter from './routes/index.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
 //정적 경로 설정
-app.use("/static", express.static("uploads"));
+app.use('/static', express.static('uploads'));
 
-app.get("/", (req, res)=>{
-  res.json({message: "바질팜 테스트"});
-});
+// 포트 연결
+const port = parseInt(process.env.PORT ?? '8080');
 
 app.listen(port, () => {
   console.log(`🚀 서버가 포트 ${port}에서 운영중입니다.`);
