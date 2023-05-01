6115a6999b8b458c00103d2c44c06f0caaf3824d
이경은 <merryjenny28@gmail.com>
2023-05-01 18:51:44 +0900

    [REFACTOR] 코드리뷰 내용 반영

--- src/clients/db.js
+++ src/clients/db.js
@@ -1,19 +1,21 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 class DB {
-  constructor({ host, user, password, database }) {
+  constructor({ 
+    host,
+    user,
+    password,
+    database
+  }) {
     this.pool = mysql.createPool({
       host,
       user,
       password,
       database,
-      waitForConnections: true,
-      connectionLimit: 10,
       maxIdle: 10,
       idleTimeout: 60000,
-      queueLimit: 0,
-    });
-    this.promisePool = this.pool.promise();
+    }); 
+
   }
   async insertData({
     idx,
@@ -24,7 +26,8 @@ class DB {
     moisture,
     created_at,
   }) {
-    const sql = `INSERT INTO sensor_history (idx, device_id, temp, humidity, light, moisture, created_at) values (?,?,?,?,?,?,?)`;
+    const sql = `INSERT INTO sensor_history 
+    (idx, device_id, temp, humidity, light, moisture, created_at) values (?,?,?,?,?,?,?)`;
     const [rows] = await this.promisePool.query(sql, [
       idx,
       device_id,
@@ -34,6 +37,7 @@ class DB {
       moisture,
       created_at,
     ]);
+    
     return rows;
   }
 }
