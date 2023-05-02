0eefb9fe1a513e9a3dfc702155103936c6428d86
이경은 <merryjenny28@gmail.com>
2023-05-02 18:29:09 +0900

    [feat] api 작업중 임시저장

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import actuatorRouter from "./actuatorRouter.js";
 import sensorRouter from "./sensorRouter.js";
+import realtimeRouter from "./realtimeRouter.js";
 
 const router = express.Router();
 
-router.use("/actuator", actuatorRouter);
-router.use("/sensor", sensorRouter);
+router.use("/realtime", realtimeRouter);
+// router.use("/sensors", sensorRouter);
+router.use("/actuators", actuatorRouter);
+router.use("/actuators/pump", actuatorRouter);
+router.use("/actuators/led", actuatorRouter);
+router.use("/actuators/fan", actuatorRouter);
 
 
 export default router;
