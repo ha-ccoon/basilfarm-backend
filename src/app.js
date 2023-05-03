bc089b1d65bb608838510eef3b7923d9fb3698bd
[IoT1] 이경은 <merryjenny28@gmail.com>
2023-05-04 03:12:09 +0900

    [HOTFIX] vm 작동을 위한 코드 수정 테스트용

--- src/app.js
+++ src/app.js
@@ -1,27 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import apiRouter from './routes/index.js';
-import DB from './mqtt-client/db.js';
-import MqttSetup from './mqtt-client/mqtt-client.js';
-import messageCallback from './mqtt-client/mqtt-controller.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import apiRouter from "./routes/index.js";
+import DB from "./dbconfig.js";
+import MqttSetup from "./mqtt-client/mqtt-client.js";
+import messageCallback from "./mqtt-client/mqtt-controller.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 app.use(cors());
 
 // 정적 경로 설정
-app.use('/static', express.static('uploads'));
+app.use("/static", express.static("uploads"));
 
 // 포트 연결
-const port = parseInt(process.env.PORT ?? '8080');
+const port = parseInt(process.env.PORT ?? "8080");
 
 app.listen(port, () => {
   console.log(`🚀 서버가 포트 ${port}에서 운영중입니다.`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // MQTT connection
@@ -32,19 +36,10 @@ const mqttOptions = {
   password: process.env.MQTT_PASSWORD,
 };
 
-const mqttClient = new MqttSetup(mqttOptions, ['data/unit002/#']);
+const mqttClient = new MqttSetup(mqttOptions, ["data/unit002/#"]);
 mqttClient.connect();
 mqttClient.subscribe();
 mqttClient.receiveMessage(messageCallback);
-
-// MySQL connection
-const database = new DB({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-});
 
 // MySQL connection 실행
 function getDBConnection() {
--- src/controller/sensorController.js
+++ src/controller/sensorController.js
@@ -1,22 +1,25 @@
-import connection from '../dbconfig.js';
+import db from "../dbconfig.js";
 
 export const getSensorData = async (req, res) => {
-    try {
-      const [rows] = await connection.execute('SELECT * FROM sensor_history');
-      res.json(rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    }
-  };
+  try {
+    const [rows] = await db.pool.query("SELECT * FROM sensor_history");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export const getSensorDataByDeviceId = async (req, res) => {
   const { device_id } = req.params;
   try {
-    const [rows] = await connection.execute('SELECT * FROM sensor_history WHERE device_id = ?', [device_id]);
+    const [rows] = await db.pool.query(
+      "SELECT * FROM sensor_history WHERE device_id = ?",
+      [device_id]
+    );
     res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 };
