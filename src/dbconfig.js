56807062f8e016e4806c29a05e4d48ea03922582
이경은 <merryjenny28@gmail.com>
2023-05-05 20:35:01 +0900

    [FEAT] 5분주기로 데이터 저

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -28,12 +28,6 @@ export default class DB {
   }) {
     const sensorsql = `INSERT INTO sensor_history 
     (idx, device_id, temp, humidity, light, water_level, moisture, created_at) values (?,?,?,?,?,?,?,?)`;
-    const realtimesql = `INSERT INTO realtime 
-    (device_id, temp, humidity, light, water_level, moisture, created_at) 
-    values (?,?,?,?,?,?,?) 
-    ON DUPLICATE KEY UPDATE 
-    temp=VALUES(temp), humidity=VALUES(humidity), light=VALUES(light), 
-    water_level=VALUES(water_level), moisture=VALUES(moisture), created_at=VALUES(created_at)`;
     const rows = await this.pool.query(sensorsql, [
       idx,
       device_id,
@@ -44,15 +38,16 @@ export default class DB {
       moisture,
       created_at,
     ]);
-    const rows2 = await this.pool.query(realtimesql, [
-      device_id,
-      temp,
-      humidity,
-      light,
-      water_level,
-      moisture,
-      created_at,
-    ]);
-    return { rows, rows2 };
+
+    return { rows };
+  }
+
+  // 5분 주기로 데이터를 저장하는 함수
+  async saveDataPeriodically(data) {
+    const interval = 5 * 60 * 1000; // 5분 간격
+    while (true) {
+      await this.insertData(data);
+      await new Promise((resolve) => setTimeout(resolve, interval));
+    }
   }
 }
--- src/routes/actuatorRouter.js
+++ src/routes/actuatorRouter.js
@@ -2,17 +2,14 @@ import express from 'express';
 import { 
   getActuatorData,
   getActuatorDataByDeviceId,
-  postPumpData,
-  postLedData,
-  postFanData
 } from '../controller/actuatorController.js';
 
 const actuatorRouter = express.Router();
 
 actuatorRouter.get('/', getActuatorData);
 actuatorRouter.get('/:device_id', getActuatorDataByDeviceId);
-actuatorRouter.post('/pump/:device_id', postPumpData);
-actuatorRouter.post('/led/:device_id', postLedData);
-actuatorRouter.post('/fan/:device_id', postFanData);
+actuatorRouter.post('/pump/:device_id');
+actuatorRouter.post('/led/:device_id');
+actuatorRouter.post('/fan/:device_id');
 
 export default actuatorRouter;
