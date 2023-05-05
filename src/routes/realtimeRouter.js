8f18c49af8057e3fcbfca20ebb491e4549075d33
이경은 <merryjenny28@gmail.com>
2023-05-05 20:42:49 +0900

    [DEL] 덜 지워진 부분 지움

--- src/routes/realtimeRouter.js
+++ src/routes/realtimeRouter.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import { getRealtimeDataByDeviceId } from '../controller/realtimeController.js';
 
 const RealtimeRouter = express.Router();
-RealtimeRouter.get('/:device_id', getRealtimeDataByDeviceId);
+RealtimeRouter.get('/:device_id');
 
 export default RealtimeRouter;
