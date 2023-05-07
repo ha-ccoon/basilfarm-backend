3b11100250bce855b24f7279c632ce2a2b25df05
minha sohn <codeminhasohn@gmail.com>
2023-05-07 13:08:55 +0900

    [PROG] 로그인 기능 구현중

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
-import express from "express";
-import actuatorRouter from "./actuatorRouter.js";
-import sensorRouter from "./sensorRouter.js";
-import realtimeRouter from "./realtimeRouter.js";
+import express from 'express';
+import actuatorRouter from './actuatorRouter.js';
+import sensorRouter from './sensorRouter.js';
+import realtimeRouter from './realtimeRouter.js';
+import userRouter from './userRouter.js';
 
 const router = express.Router();
 
-router.use("/realtime", realtimeRouter);
-router.use("/sensors", sensorRouter);
-router.use("/actuators", actuatorRouter);
-router.use("/actuators/pump", actuatorRouter);
-router.use("/actuators/led", actuatorRouter);
-router.use("/actuators/fan", actuatorRouter);
+router.use('/realtime', realtimeRouter);
+router.use('/sensors', sensorRouter);
+router.use('/actuators', actuatorRouter);
+router.use('/actuators/pump', actuatorRouter);
+router.use('/actuators/led', actuatorRouter);
+router.use('/actuators/fan', actuatorRouter);
+router.use('/user', userRouter);
 
 export default router;
