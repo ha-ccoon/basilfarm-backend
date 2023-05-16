7c5fe008bbc78e6bca07cfa4527f35c5a3c86f8a
이경은 <merryjenny28@gmail.com>
2023-05-16 18:25:59 +0900

    [FEAT] DB에 저장, 자동화 중단명령 작동 확인

--- src/database.js
+++ src/database.js
@@ -41,25 +41,25 @@ export default class DB {
     return { row };
   }
 
-  // // 자동 제어 상태 저장
-  // async insertAutoStatus({
-  //   device_id,
-  //   status,
-  //   target_temp,
-  //   target_moisture,
-  //   target_light,
-  //   created_at,
-  // }) {
-  //   const formattedCreatedAt = new Date(created_at).toISOString().slice(0, 19).replace('T', ' ');
+  // 자동 제어 상태 저장
+  async insertAutoStatus({
+    device_id,
+    status,
+    target_temp,
+    target_moisture,
+    target_light,
+    created_at,
+  }) {
+    const formattedCreatedAt = new Date(created_at).toISOString().slice(0, 19).replace('T', ' ');
 
-  //   const sql = 
-  //     `INSERT INTO auto_status (device_id, status, target_temp, target_moisture, target_light, created_at)
-  //     VALUES (${this.pool.escape(device_id)}, ${this.pool.escape(status)}, ${this.pool.escape(target_temp)}, ${this.pool.escape(target_moisture)}, ${this.pool.escape(target_light)}, '${formattedCreatedAt}')
-  //     ON DUPLICATE KEY UPDATE status = ${status}, target_temp = ${target_temp}, target_moisture = ${target_moisture}, target_light = ${target_light}, created_at = '${formattedCreatedAt}';`
+    const sql = 
+      `INSERT INTO auto_status (device_id, status, target_temp, target_moisture, target_light, created_at)
+      VALUES (${this.pool.escape(device_id)}, ${this.pool.escape(status)}, ${this.pool.escape(target_temp)}, ${this.pool.escape(target_moisture)}, ${this.pool.escape(target_light)}, '${formattedCreatedAt}')
+      ON DUPLICATE KEY UPDATE status = ${status}, target_temp = ${target_temp}, target_moisture = ${target_moisture}, target_light = ${target_light}, created_at = '${formattedCreatedAt}';`
 
-  //   const [rows] = await this.pool.query(sql);
-  //   return rows;
-  // }
+    const [rows] = await this.pool.query(sql);
+    return rows;
+  }
 
   // actuator 현재 상태 데이터 저장
   async insertActuatorConfig({
--- src/routes/auto.js
+++ src/routes/auto.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { autoManagement, saveAutoStatus, currentAutoManagementStatus } from '../controller/auto.js';
-//saveAutoStatus,
+import { saveAutoStatus, autoManagement, currentAutoManagementStatus } from '../controller/auto.js';
 
 const router = express.Router();
 
-router.post('/:device_id', autoManagement, saveAutoStatus);
+router.post('/:device_id', saveAutoStatus,autoManagement);
 router.get('/:device_id/status', currentAutoManagementStatus);
 
 export default router;
+
+// saveAutoStatus
