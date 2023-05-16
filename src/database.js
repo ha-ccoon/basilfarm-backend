5c0c6b8e391940655b1977ee279dee41ed9f89c8
이경은 <merryjenny28@gmail.com>
2023-05-16 10:03:53 +0900

    [FEAT] 자동화 명령 전송 테스트 완료

--- src/database.js
+++ src/database.js
@@ -41,26 +41,25 @@ export default class DB {
     return { row };
   }
 
-  // 자동 제어 상태 저장
-  async insertAutoStatus({
-    device_id,
-    status,
-    target_temp,
-    target_moisture,
-    created_at,
-  }) {
-    const sql = 
-      `INSERT INTO auto_status (device_id, status, target_temp, target_moisture, created_at)
-      VALUES (${this.pool.escape(device_id)}, ${this.pool.escape(status)}, ${this.pool.escape(target_temp)}, ${this.pool.escape(target_moisture)}, ${this.pool.escape(created_at)})
-      ON DUPLICATE KEY UPDATE 
-      status = VALUES(status),
-      target_temp = VALUES(target_temp),
-      target_moisture = VALUES(target_moisture),
-      created_at = VALUES(created_at);`
-  
-    const row = await this.pool.query(sql);
-    return { row };
-  }  
+  // // 자동 제어 상태 저장
+  // async insertAutoStatus({
+  //   device_id,
+  //   status,
+  //   target_temp,
+  //   target_moisture,
+  //   target_light,
+  //   created_at,
+  // }) {
+  //   const formattedCreatedAt = new Date(created_at).toISOString().slice(0, 19).replace('T', ' ');
+
+  //   const sql = 
+  //     `INSERT INTO auto_status (device_id, status, target_temp, target_moisture, target_light, created_at)
+  //     VALUES (${this.pool.escape(device_id)}, ${this.pool.escape(status)}, ${this.pool.escape(target_temp)}, ${this.pool.escape(target_moisture)}, ${this.pool.escape(target_light)}, '${formattedCreatedAt}')
+  //     ON DUPLICATE KEY UPDATE status = ${status}, target_temp = ${target_temp}, target_moisture = ${target_moisture}, target_light = ${target_light}, created_at = '${formattedCreatedAt}';`
+
+  //   const [rows] = await this.pool.query(sql);
+  //   return rows;
+  // }
 
   // actuator 현재 상태 데이터 저장
   async insertActuatorConfig({
--- src/routes/auto.js
+++ src/routes/auto.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { saveAutoStatus, autoManagement, getAutoManagementStatus } from '../controller/auto.js';
+import { autoManagement, saveAutoStatus, currentAutoManagementStatus } from '../controller/auto.js';
+//saveAutoStatus,
 
 const router = express.Router();
 
-router.post('/:device_id', saveAutoStatus, autoManagement);
-router.get('/:device_id/status', getAutoManagementStatus);
+router.post('/:device_id', autoManagement, saveAutoStatus);
+router.get('/:device_id/status', currentAutoManagementStatus);
 
 export default router;
