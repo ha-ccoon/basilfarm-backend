077fcb6d6819029cbba2d9906615602ee43b0143
minha sohn <codeminhasohn@gmail.com>
2023-06-15 20:59:04 +0900

    [FEAT] 토큰을 쿠키에 담아 보내는 것으로 변경

--- src/controller/users/users.js
+++ src/controller/users/users.js
@@ -5,7 +5,7 @@ const db = new DB();
 // 아이디 검색
 export const findById = async (id) => {
   try {
-    const [row] = await db.pool.query('SELECT * FROM user WHERE id = ?', [id]);
+    const [row] = await db.pool.query('SELECT * FROM users WHERE id = ?', [id]);
     console.log('아이디 검색: ', row);
     return row;
   } catch (err) {
@@ -17,7 +17,7 @@ export const findById = async (id) => {
 export const findByPassword = async (id) => {
   try {
     const [row] = await db.pool.query(
-      'SELECT password FROM user WHERE id = ?',
+      'SELECT password FROM users WHERE id = ?',
       [id]
     );
     return row;
@@ -32,7 +32,7 @@ export const getUserInfo = async (req, res) => {
   const userId = req.id;
   try {
     const [row] = await db.pool.query(
-      'SELECT id, phone, email, fullname, picture, device_id, created_at FROM user WHERE id = ?',
+      'SELECT id, phone, email, fullname, picture, device_id, created_at FROM users WHERE id = ?',
       [userId]
     );
     res.json(row);
@@ -40,13 +40,3 @@ export const getUserInfo = async (req, res) => {
     res.status(500).json({ message: 'Database Error' });
   }
 };
-
-// 유저 정보 수정
-// export const updateUser = async (req, res) => {
-//   const { id, password, phone, email, device_id } = req.body;
-//   try {
-//     const [row] = await db.pool.query(
-//       'UPDATE id, password, phone, email, device_id SET id = ?, password = ?, phone = ?, email = ?, '
-//     )
-//   } catch (err) {}
-// };
